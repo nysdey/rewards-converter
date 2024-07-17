a1f55d5c6462ffae0a1ea94abe092975f5071e03
nysdey <sydneychin13@gmail.com>
2024-07-17 15:18:21 -0400

    fixed again i think

--- src/main/RewardValue.java
+++ src/main/RewardValue.java
@@ -3,20 +3,24 @@ package main;
 public class RewardValue {
     private double cashValue;
     private int milesValue;
+    public static final double conversionRate = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
-
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
+        this.cashValue = convertToCash(milesValue);
     }
-
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / conversionRate);
+    }
+    private static double convertToCash(int milesValue) {
+        return milesValue * conversionRate;
+    }
     public double getCashValue() {
         return cashValue;
     }
-
     public int getMilesValue() {
-        return milesValue;
+        return convertToMiles(this.cashValue);
     }
 }
